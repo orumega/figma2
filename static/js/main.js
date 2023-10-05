306c99b614c558736a732449e6e621f42216dc81
orumega <144137724+orumega@users.noreply.github.com>
2023-10-05 16:11:39 +0300

    tabs

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,30 @@
 //Burger
 
-const burgerMenu = document.querySelector(
-  '.header__burger',
-);
+const burgerMenu = document.querySelector('.header__burger',);
+
 if (burgerMenu) {
   const navBody = document.querySelector('.header__nav');
   burgerMenu.addEventListener('click', function (e) {
     document.body.classList.toggle('_lock');
     burgerMenu.classList.toggle('_active');
     navBody.classList.toggle('_active');
+    
   });
 }
 
 //Sound
 
-const soundOn = document.querySelector(
-  '.main-content__btn-sound',
-);
+const soundOn = document.querySelector('.main-content__btn-sound',);
 const video = document.querySelector('video');
 
 soundOn.addEventListener('click', function (e) {
-  if (video.getAttribute('muted')) {
-    alert(1);
+  if(video.muted) {
+    video.muted = false 
+  } else {
+    video.muted = true
   }
+  
+  
 });
 
 // popup
@@ -49,6 +51,70 @@ popupClose.addEventListener('click', function (e) {
   popup.classList.toggle('open');
   toggleBodyScroll();
 });
+
+//tabs
+
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn')
+const tabsItem = document.querySelectorAll('.tabs__item')
+
+tabsBtn.forEach(function (item) {
+    item.addEventListener('click', function () {
+        let curentBtn = item
+        let tabId = curentBtn.getAttribute('data-tab')
+        let curentTab = document.querySelector(tabId)
+        
+            curentBtn.classList.toggle('active')
+            curentTab.classList.toggle('active')
+
+            // tabsBtn.forEach(function (item) {
+            //     item.classList.remove('active')
+            // })
+    
+            // tabsItem.forEach(function (item) {
+            //     item.classList.remove('active')
+            // })
+    
+    
+            // curentBtn.classList.add('active')
+            // curentTab.classList.add('active')
+        
+    })
+}) 
+
+document.querySelector('.tabs__nav-btn').click()
+
+const secTabsBtn = document.querySelectorAll('.tabs__nav-btn_2')
+
+
+
+secTabsBtn.forEach(function (item) {
+    item.addEventListener('click', function () {
+        let secCurentBtn = item
+
+        secTabsBtn.forEach(function (item) {
+                item.classList.remove('active')
+        })
+
+        secCurentBtn.classList.add('active')
+    })
+})
+
+
+
+
+
+// Swiper
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+});
+
 
 //Popup
 
