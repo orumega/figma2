f907114e3a0c35da7e232587e89569f3ee1fc394
orumega <144137724+orumega@users.noreply.github.com>
2023-10-05 18:33:21 +0300

    done

--- static/js/main.js
+++ static/js/main.js
@@ -107,12 +107,7 @@ secTabsBtn.forEach(function (item) {
 
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
+    
 });
 
 
